57100aad807dff825843629f4f876a437a93d0f0
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2021-07-24 01:11:26 +0900

    20210724

---
+++ nodeJs/syntax/number.js
@@ -0,0 +1,4 @@
+console.log(1+1);
+console.log(4-1);
+console.log(5*3);
+console.log(10/2);
---
+++ nodeJs/syntax/template.js
@@ -0,0 +1,3 @@
+var name='kd4954';
+var letter= `Dear\n${name}\n\nhello my name is yeonwoo`;
+console.log(letter);
