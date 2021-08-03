f762960e2d8013b76d9d1d71574419be86f7afd8
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2021-08-03 18:30:36 +0900

    nodeBook

---
+++ chapter3/3ch/func.js
@@ -0,0 +1,10 @@
+const{ odd, even} = require('./var');
+
+function checkOddorEven(num) {
+  if(num%2){
+    return odd;
+  }
+  return even;
+}
+
+module.exports = checkOddorEven;
---
+++ chapter3/4ch/require.js
@@ -0,0 +1,10 @@
+console.log('require가 가장 위로 오지 않아도 된다.');
+
+module.exports = '저를 찾아보세요.';
+require('./var');
+
+console.log('require.cache');
+console.log(require.cache);
+console.log('require.main');
+console.log(require.main === module);
+console.log(require.main.filename);
