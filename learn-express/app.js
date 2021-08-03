58f6aabb771bf56d737d27406f30478e2dd59fda
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2021-08-03 18:31:55 +0900

    nodeBook
    no module files

---
+++ learn-express/app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
+
+
+dotenv.config();
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user')
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended : false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave : false,
+  saveUninitialized : false,
+  secret : process.env.COOKIE_SECRET,
+  cookie : {
+    httpOnly : true,
+    secure : false,
+  },
+  name : "session-cookie",
+}));
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+app.use((req, res, next)=>{
+  res.status(404).send('Not Found');
+});
+
+const multer = require('multer');
+const fs = require('fs');
+
+try{
+  fs.readdirSync('uploads');
+
+}catch(error){
+  console.error('uploads 폴더가 없어 폴더 생성');
+  fs.mkdirSync('uploads');
+}
+const upload = multer({
+  storage : multer.diskStorage({
+    destination(req, file, done){
+      done(null, 'uploads/');
+    },
+    filename(req, file, done){
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext)+Data.now()+ext);
+    },
+  }),
+  limits : { filesize : 5* 1024 * 1024},
+});
+app.get('/upload', (req, res) => {
+  res.sendFile(path.join(__dirname, 'multipart.html'));
+});
+app.post('/upload', upload.fields([{ name : 'image1'}, { name : 'image2'}]),
+  (req, res) =>{
+    console.log(req.files, req.body);
+    res.send('ok');
+  },
+);
+
+//app.use((req, res, next) => {
+  //console.log('모든 요청에서 실행');
+  //next();
+//});
+app.get('/',(req, res, next) => {
+  console.log('GET / 요청에서 실행');
+  next();
+}, (req, res) => {
+  throw new Error('에러는 에러 처리 미들웨어로 감');
+});
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});
